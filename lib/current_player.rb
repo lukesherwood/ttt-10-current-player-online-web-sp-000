fcc81c8b8c3ba773e4b429748bd1d8e0ce5a42cc
Luke Sherwood <luke.sherwood55@gmail.com>
2019-12-10 18:12:27 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,12 +1,11 @@
 def turn_count(board)
   count = 0
   board.each do |turn|
-    if turn == 'X'|turn == 'O'
+    if turn == "X"|turn == "O"
     count +=1
     end
   end
   return count
-  
   
 end
 
